0021923b3efa9b8667ebf0d63b1fe18888c44fd8
andy silva <andssilva.23@gmail.com>
2021-05-15 10:15:06 -0400

    feat (requirement 1): :rocket: Creates route for Pokedéx, fixes images in the data.js file

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
 import pokemons from './data';
 import Pokedex from './Pokedex';
@@ -7,7 +8,9 @@ function App() {
   return (
     <div className="App">
       <h1> Pokedex </h1>
-      <Pokedex pokemons={pokemons} />
+      <BrowserRouter>
+        <Route exact path="/" render={() => <Pokedex pokemons={pokemons} />} />  
+      </BrowserRouter>
     </div>
   );
 }
