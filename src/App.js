129efad00621afd03a310aab57ce824dd1704dec
andy silva <andssilva.23@gmail.com>
2021-05-15 10:39:34 -0400

    feat (requirement 2): :rocket: Creates the navigation link for the details of the Pokemon

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
 import pokemons from './data';
 import Pokedex from './Pokedex';
+import Pokemon from './Pokemon';
 
 function App() {
   return (
     <div className="App">
       <h1> Pokedex </h1>
       <BrowserRouter>
-        <Route exact path="/" render={() => <Pokedex pokemons={pokemons} />} />  
+        <Route exact path="/" render={() => <Pokedex pokemons={pokemons} />} />
+        <Route exact path="/pokemons" render={(props) => <Pokemon {...props} />} />
+        <Route exact path="/pokemons/:id" render={(props) => <PokemonDetails {...props} />} />  
       </BrowserRouter>
     </div>
   );
