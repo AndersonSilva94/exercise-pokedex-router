b533b3968227bb48ee32815c00ae19112ca9c7cf
andy silva <andssilva.23@gmail.com>
2021-05-15 12:08:10 -0400

    feat (requirement 7): :rocket: Creates a component NotFound

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import About from './About';
 import './App.css';
 import pokemons from './data';
 import Header from './Header';
 import Pokedex from './Pokedex';
+import NotFound from './NotFound';
 //import Pokemon from './Pokemon';
 import PokemonDetails from './PokemonDetails';
 
@@ -13,10 +14,13 @@ function App() {
     <BrowserRouter>
       <div className="App">
         <Header />
-        <Route exact path="/" render={() => <Pokedex pokemons={pokemons} />} />
-        {/* <Route exact path="/pokemons" render={(props) => <Pokemon {...props} />} /> */}
-        <Route exact path="/about" component={ About } />
-        <Route exact path="/pokemons/:idPoke" render={(props) => <PokemonDetails {...props}  pokemons={ pokemons } />} />  
+        <Switch>
+          <Route exact path="/" render={() => <Pokedex pokemons={pokemons} />} />
+          {/* <Route exact path="/pokemons" render={(props) => <Pokemon {...props} />} /> */}
+          <Route exact path="/about" component={ About } />
+          <Route exact path="/pokemons/:idPoke" render={(props) => <PokemonDetails {...props}  pokemons={ pokemons } />} />  
+          <Route path="*" component={ NotFound } />
+        </Switch>
       </div>
     </BrowserRouter>
   );
